56dea2aea7ead9379cc89c08931067d8d12e8748
Noitidart <noitidart@gmail.com>
2020-01-11 17:11:59 -0800

    Fill in text content provided

--- src/App/index.js
+++ src/App/index.js
@@ -1,25 +1,144 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Libyan Americans for Human Rights
+        <br/>
+        <br/>
+        <b>EMPOWERMENT THROUGH HUMAN RIGHTS</b>
       </header>
+
+        <div className="Section">
+          <div className="Section--para">
+            Lending a hand for Libyans
+            <br/><br/>
+            Human Rights, Rule of Law, and Access to Justice represent the world’s best hope for building peaceful, prosperous societies.
+            <br/><br/>
+            Advancing human rights by respecting civil, political, economic, social and cultural rights is a pillar of a strong rule of law culture, providing safety and stability to communities and nations alike.
+            <br/><br/>
+            Believing in the role assigned to all of us to advance the democratic process, preserve the freedom of all citizens and stand up to injustice the Libyan Americans for Human Rights ask you to support our goals, efforts and our achievements. We strongly believe that your contributions within your capacity, whether it may be financially or otherwise, will make a huge impact on peace, justice, prosperity and stability in Libya and in the surrounding. As is well known that USA has the most important influence in Libyan’s stability. Therefore, we need your engagement with your senator and member of congress to sponsor and support "The Libyan Stabilization Act".
+          </div>
+        </div>
+
+        <div className="Section">
+          <h3 className="Section--title">Mission Statement:</h3>
+          <div className="Section--para">
+            Libyan Americans for Human Rights is a non-profit organization working to promote full civil and human rights specifically in Libya as well as worldwide.
+            <br/><br/>
+            LAHR advocates the protection of civilians during armed conflicts sates in international humanitarian law and refugee law that share a common goal, the protection of the lives, health and dignity of persons.
+            <br/><br/>
+            LAHR activities are be based on the main international and regional instruments for the protection of human rights. These instruments preserve common values regarding fundamental freedoms and democratic principles, which are universal, indivisible and interdependent.
+          </div>
+        </div>
+
+        <div className="Section">
+          <h3 className="Section--title">Vision:</h3>
+          <div className="Section--para">
+            To create a transparent, efficient and accessible justice system for all Libyans and other people, especially the poor and members of vulnerable and disadvantaged groups of society.
+          </div>
+        </div>
+
+        <div className="Section">
+          <h3 className="Section--title">We are pursuing four goals this year</h3>
+
+          <ol>
+            <li>Filing a lawsuit of war crimes Hafter</li>
+            <li>
+              <ol>
+                <li>We have filed a civil lawsuit against Hafter on June 26, which brought media attention and interest including Al-Jazeera and the Associated Press, that led to publication of the case in most of the world newspapers.</li>
+                <li>We are expecting the court will pronounce the judgement in the next few weeks.</li>
+                <li>The challenge, is to require payment of large sums of money to investigate all Haftar’s financial asset, properties and funds.</li>
+              </ol>
+            </li>
+          </ol>
+
+          <ol>
+            <li>Documenting war crimes, human rights violations, preparing reports and research</li>
+            <li>
+              <ol>
+                <li>The importance of documenting war crimes and human rights violations in building a state of law, political stability and democracy</li>
+                <li>When we document and report human rights violations, we build a case.</li>
+                <li>To build an issue we need many examples and many details to be reliable.</li>
+                <li>Accuracy of information is the single most important factor in human rights documents. Whether quoting from a law, quoting a victim’s testimony or reporting something that is already in the media</li>
+                <li>It is extremely important to verify our facts and present them accurately.</li>
+              </ol>
+            </li>
+          </ol>
+
+          <div className="Section--para">
+            The basic principles for documenting human rights violations and generally consist of:
+            <ol>
+              <li>Data collection</li>
+              <li>Data organization</li>
+              <li>Analyze the data to make it easier.</li>
+              <li>Advocacy and publishing depend on an accurate strategy.</li>
+            </ol>
+          </div>
+
+          <ol>
+            <li>Second: - Supporting "Libyan Stabilization Act" congressional Bill both by the House and the Senate as an urgent goal must be achieved in the coming few weeks.</li>
+            <li>To support the project, an urgent campaign is required as shown below:</li>
+            <li>
+              <ol>
+                <li>Communicate with the Libyan communities to pressure their members of Congress in their electoral areas</li>
+                <li>Reserve a place in one of the chambers of Congress to convince other members of Congress to vote in favor of the draft resolution, where we will make a presentation on the ongoing war crimes in Libya</li>
+                <li>Communicating with human rights organizations</li>
+              </ol>
+            </li>
+            <li>
+              <b>
+                Notice:
+                <br/><br/>
+                Amnesty International has been persuaded to support the draft resolution persuade them to come and participate and talk about war crimes. Huma Rights Watch has also been persuaded to attend and participate in thi program.
+                <br/><br/>
+                Supporting these human rights organizations has the effect of persuading other members of Congress
+              </b>
+            </li>
+          </ol>
+
+          <ol>
+            <li>Education and Awareness Project for the Libyan Americans, Arab Americans, Muslim Americans in legal, civil and Democratic advocacy in United States of America</li>
+            <li>
+              The importance of spreading awareness in our communities about their responsibilities towards human and civil work and communicating with officials and decision-makers, especially members of Congress in their electoral constituencies. The prosperity of democracy, human rights and the rule of law requires the activity of members of the community and all citizens of Libya
+              <br/><br/>
+              The human rights education and training can be a key component of national peacebuilding. The range of human rights activities associated with truth, reconciliation and transitional justice, the national and international activities designed to support the rebuilding of the national government, its institutions and social services, as well as other activities directed towards reestablishing the national economy. Within this complex, human rights education is typically unique because it alone focuses on the daily lives of the citizenry at large.
+              <br/><br/>
+              <b><i>Democracy and Human Rights</i></b>
+              <br/><br/>
+              In general principle, promoting democracy and supporting human rights are interrelated, interdependent, and inseparable concepts.  Although democratic societies can protect and advance human rights better than other forms of government, nevertheless the promotion of human rights even under repressive regimes or conflict can advance their transformation toward democracy.
+              <br/><br/>
+              Therefore, the Libyan American Human Rights Organization will seek to conduct several workshops in this field in ten American cities.
+              <br/><br/>
+              It is also seeking to set up these workshops in Tunisia since Amnesty International agreed to participate in these workshops.
+              <br/><br/>
+              <b>Note:</b> There were joint workshops conducted before by the Libyan American Human Rights Organization with Amnesty International in 2005.
+            </li>
+          </ol>
+
+          <div className="Section">
+            <h3 className="Section--title">Achievements</h3>
+            <ol>
+              <li>LAHR and Libyan American Alliance have filed a lawsuit against Khalifa Haftar’s war crimes on behalf of three Libyan families.</li>
+              <li>LAHR and Amnesty International USA (AIUSA) jointly conducted workshops 2005</li>
+              <li>LAHR Advised Human Rights Watch to visit and report on conditions of Abu Salim prison 2005</li>
+              <li>Amnesty International agreed to advocate human rights issues in the "Libyan Stabilization Act" by the house and the senate.</li>
+              <li>Human Rights Watch agreed to support human rights issues in the "Libyan Stabilization Act”</li>
+              <li>Amnesty International USA has agreed to join LAHR to organize training workshops in several cities in USA and Tunisia.</li>
+            </ol>
+          </div>
+        </div>
+
+        <div className="Section">
+          <h3 className="Section--title">What we need?</h3>
+
+          <div className="Section--para">
+            Believing in the role assigned to all of us to advance the democratic process, preserve the freedom of all citizens and stand up to injustice, LAHR ask you to support our goals and efforts. We strongly believe that your contributions within your capacity, whether it may be financially or otherwise, will make a huge impact on peace, justice, prosperity and stability in Libya and elsewhere.
+          </div>
+        </div>
     </div>
   );
 }
